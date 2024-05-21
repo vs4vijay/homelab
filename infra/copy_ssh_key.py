3970680bed1ef67b8a391000930e5e054052d910
vs4vijay <vs4vijay@gmail.com>
2024-05-21 23:07:19 +0530

    changes

--- infra/copy_ssh_key.py
+++ infra/copy_ssh_key.py
@@ -1,29 +1,16 @@
-import subprocess
+from pyinfra import local, host
+from pyinfra.operations import files, ssh
 
-# Define the target servers
-source_server = 'source_server'
-destination_server = 'destination_server'
+# Define the target host
+target_host = host
 
-# SSH command to list public keys on the source server
-list_keys_command = f'ssh {source_server} "cat ~/.ssh/authorized_keys"'
 
-# Execute the command and capture the output
-output = subprocess.check_output(list_keys_command, shell=True, universal_newlines=True)
+# Generate the SSH key pair locally
+local.shell('ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa1')
 
-# Split the output into individual keys
-keys = output.strip().split('\n')
-
-# Display the keys to the user
-print('Available public keys:')
-for i, key in enumerate(keys):
-    print(f'{i+1}. {key}')
-
-# Prompt the user to choose a key
-selected_key_index = int(input('Enter the number of the key to copy: ')) - 1
-selected_key = keys[selected_key_index]
-
-# SSH command to copy the selected key to the destination server
-copy_key_command = f'ssh {destination_server} "echo \'{selected_key}\' >> ~/.ssh/authorized_keys"'
-
-# Execute the command to copy the key
-subprocess.run(copy_key_command, shell=True)
+# Copy the public key to the target host
+ssh.authorized_key(
+    name="Copy SSH public key",
+    user=target_host.user,
+    key="~/.ssh/id_rsa1.pub",
+)
