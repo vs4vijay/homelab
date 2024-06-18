5eaee409e9990eebfc1c41738af91785e84cafc7
vs4vijay <vs4vijay@gmail.com>
2024-06-18 22:35:15 +0530

    changes

---
+++ infra/harden_server.py
@@ -0,0 +1,30 @@
+from pyinfra import host
+from pyinfra.operations import apt, server
+
+
+# Change SSH port to 22222
+server.shell(
+    name='Change SSH port',
+    commands=[
+        'sed -i "s/#Port 22/Port 22222/" /etc/ssh/sshd_config',
+        'systemctl restart sshd',
+    ],
+)
+
+# Disable password authentication and allow only public key authentication
+server.shell(
+    name='Disable password authentication',
+    commands=[
+        'sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config',
+        'systemctl restart sshd',
+    ],
+)
+
+# Disable root login
+server.shell(
+    name='Disable root login',
+    commands=[
+        'sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/" /etc/ssh/sshd_config',
+        'systemctl restart sshd',
+    ],
+)
